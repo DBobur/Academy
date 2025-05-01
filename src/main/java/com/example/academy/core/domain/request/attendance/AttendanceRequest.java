ca4d1d83d672cddb76ccc5e4a59ef071d3996a63
DBobur <davlatboyev2024@gmail.com>
2025-05-01 11:29:02 +0500

    Remove .env from tracking

--- src/main/java/com/example/academy/core/domain/request/attendance/AttendanceRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.academy.core.domain.request.attendance;
-
-import lombok.Data;
-
-@Data
-public class AttendanceRequest {
-    private Long teacherId;
-    private Long scheduleId;
-}
